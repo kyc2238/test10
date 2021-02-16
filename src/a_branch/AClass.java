1321ab3de0d96b15f91f7763f6d322860b2b839b
kyc <kyc@kakao.com>
2021-02-16 13:29:27 +0900

    로그인기능까지만

---
+++ src/a_branch/AClass.java
@@ -0,0 +1,8 @@
+package a_branch;
+
+public class AClass {
+		public AClass() {
+			System.out.println("a가 만든 기능.");
+		}
+	
+}
--- src/main/mainClass.java
+++ src/main/mainClass.java
@@ -4,5 +4,6 @@ public class mainClass {
 
 	public static void main(String[] args) {
 		System.out.println("공통 내용 틀 입니다.");
+		
 	}
 }
